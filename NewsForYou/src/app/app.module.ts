46190c8b5f4e48a453e09cb4ee801014d2ad3942
Mal Ensor <Mal.Ensor@vu.com>
2020-03-27 15:39:06 -0500

    Added content for landing page, scaffolding for animation

--- NewsForYou/src/app/app.module.ts
+++ NewsForYou/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ NewsForYou/src/app/welcome/welcome.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, state, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+      state('in', style({opacity: 1})),
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(600 )
+      ]),
+      transition(':leave',
+        animate(600, style({opacity: 0})))
+    ])
+  ]
+})
+export class WelcomeComponent implements OnInit {
+  count = 0;
+  narrative: string[] = [
+    'I know things have been...strange.',
+    'And we haven\'t seen each other in awhile',
+    'But I\'ve been thinking about you',
+    'And I wanted to make you smile',
+    'So I put this together for you'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Increment() {
+    if (this.count < 4) {
+      this.count++;
+      console.log(this.count);
+    }
+  }
+}
