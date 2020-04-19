f4be88a688fa0baea218e14caed09930ef1d0a5b
malenso <mfensor@gmail.com>
2020-04-19 14:41:44 -0500

    Exclude json files, only return articles in english
    Feature: add customizable language settings

--- NewsForYou/src/app/services/news/news.service.ts
+++ NewsForYou/src/app/services/news/news.service.ts
@@ -3,25 +3,25 @@ import { HttpClient } from '@angular/common/http';
 
 import { Article } from 'src/app/models/article';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'; 
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class NewsService {
-  
+
   constructor(
     private http: HttpClient
   ) { }
 
   GetArticles(topic: string): Observable<Article[]> {
-    const key = '';
-    const url = `https://newsapi.org/v2/top-headlines?q=${topic}&apiKey=${key}`;
+    const key = 'fc2a4aa033814df2adaa6f9ab9ada05d';
+    const url = `https://newsapi.org/v2/top-headlines?q=${topic}&apiKey=${key}&language=en`;
 
-    return this.http.get(url)
+    return this.http.get<Article[]>(url)
       .pipe(
-        map(response => response["articles"])
+        map(response => response["articles"]) //this feels off
       );
   }
 }
